1441bbf1b10a2e96a053b3871d3ae483682449b0
Gyanendra Gaurav <gyanendragaurav32@gmail.com>
2025-07-01 21:45:06 +0530

    "Initial commit"

--- app/src/main/java/com/example/cometchat/MainActivity.kt
+++ app/src/main/java/com/example/cometchat/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cometchat.chatscreen.ChatContent
 import com.example.cometchat.homescreen.HomeScreen
 import com.example.cometchat.ui.theme.CometChatTheme
 
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CometChatTheme {
-                HomeScreen()
+                ChatContent()
             }
         }
     }
